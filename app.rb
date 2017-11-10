49258f9f647a8a526ef9cb12a7831be7b9397bc5
Justin Tooley <jstooley823@gmail.com>
2017-11-10 19:49:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ class App < Sinatra::Base
   "My name is "
 end
 get '/hometown' do
-  "My hometown is"
+  "My hometown is "
 end
 get '/favorite-song' do
-  "My favorite song is"
+  "My favorite song is "
 end
 end
